7b03bc4d130fccd9eb05087598a2bc7acfdfb1fb
Rhishav <rhishh.me@gmail.com>
2024-07-28 13:25:17 +0530

    main updates

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,23 @@
+'use client';
+import { motion,useScroll,useTransform } from "framer-motion";
+
+
 import { cn } from "@/lib/utils";
 import GridPattern from "@/components/GridPattern";
 
+
 import Header from "@/components/Header";
+import ScrollText from "@/components/ScrollText";
+
 
 export default function Home() {
+
+  const {scrollY}=useScroll();
+
+  const hoverRight=useTransform(scrollY,[0,500],[390,1500]);
+  const hoverLeft=useTransform(scrollY,[0,300],[590,1000]);
+  // const hoverSize=useTransform(scrollY,[0,500],[40,100]);
+
   return ( <>
       <GridPattern
         squares={[
@@ -22,11 +36,14 @@ export default function Home() {
         ]}
         className={cn(
           "[mask-image:radial-gradient(800px_circle_at_center,white,transparent)]",
-          "inset-x-0 inset-y-[-30%] h-[200%] opacity-25 -z-50",
+          "inset-x-0 inset-y-[-30%] h-[200%] opacity-50 -z-50",
         )}
       />
       <Header/>
-    <main className="flex min-h-screen flex-col items-center p-24">
+
+      
+
+      <main className="flex min-h-screen flex-col items-center p-24 relative">
         <p className="font-anton text-6xl m-2">
           BORED OF TAKING NOTES ?
         </p>
@@ -34,8 +51,26 @@ export default function Home() {
           Introducing MEMO
         </p>
         <p className="font-poppins text-xl mt-5">
-          Not the notes app you need but the one <span className="font-anton"> you want !</span>
+          Not the notes app you need but the one <span className="font-anton"> you deserve !</span>
         </p>
+
+        <div className="flex mt-20 items-center gap-6 font-noto text-xl">
+          <button className="bg-orange-600 rounded-xl p-2 pl-4 pr-4 outline-none
+          hover:bg-orange-500 transition-all ease-in-out duration-300">
+            Get Started</button>
+            <button className="rounded-xl p-2 pl-4 pr-4 outline-none
+           bg-white/30 hover:bg-white/50 transition ease-in-out duration-300">
+            Log In</button>
+        </div>
+
+        <motion.div className="bg-orange-600/50 size-28 rounded-full absolute top-48 backdrop-invert z-10"
+        style={{right:hoverRight}}
+        ></motion.div>
+        <motion.div className="bg-red-600/50 size-10 rounded-full absolute top-28 backdrop-invert z-10"
+        style={{left:hoverLeft}}
+        ></motion.div>
+
+        <ScrollText/>
         
     </main>
     </>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,10 @@
 export default function Header(){
   return(
-    <header className='flex justify-between items-center m-4'>
+    <header className='flex justify-between items-center m-4 z-40'>
       <div className='flex justify-center items-center'>
         <img className='w-8 m-3' src={'SNlogo.svg'} alt="" />
-        <p className='text-white '>Memo</p>
+        <p className='text-white cursor-pointer'>
+          Memo</p>
       </div>
       <nav className='flex gap-10 '>
         <p className='hover:scale-110 transition ease-in-out duration-300 cursor-pointer'>
@@ -13,9 +14,10 @@ export default function Header(){
         {/* <p className='hover:scale-125 transition ease-in-out duration-300'>
           Why Script Nest</p> */}
       </nav>
-      <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center cursor-pointer'>
         <img className='w-8' src={'github.svg'} alt="" />
-        <p className='m-3'>Connect Me</p>
+        <p className='m-3'>
+          Connect Me</p>
 
       </div>
     </header>
