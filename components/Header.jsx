41b6e94ba6bc417e9418d9dfc6878831ec0b8805
Rhishav <rhishh.me@gmail.com>
2024-08-09 18:12:06 +0530

    header

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,20 +6,22 @@ export default function Header(){
     <header className='flex justify-between items-center m-4 z-40'>
       <div className='flex justify-center items-center'>
         <Image className='m-3' src='SNlogo.svg' alt="" width={30} height={30} />
-        <p className='text-white cursor-pointer'>
+        <p className='font-hand text-3xl text-white cursor-pointer'>
           Memo</p>
       </div>
       <nav className='flex gap-10 '>
-        <p className='hover:scale-110 transition ease-in-out duration-300 cursor-pointer'>
+        <p className='font-hand text-3xl
+        hover:scale-110 transition ease-in-out duration-300 cursor-pointer'>
           Home</p>
-        <p className='hover:scale-110 transition ease-in-out duration-300 cursor-pointer'>
+        <p className='font-hand text-3xl
+        hover:scale-110 transition ease-in-out duration-300 cursor-pointer'>
           <Link href="/dashboard">Dashboard</Link></p>
         {/* <p className='hover:scale-125 transition ease-in-out duration-300'>
           Why Script Nest</p> */}
       </nav>
       <div className='flex justify-center items-center cursor-pointer'>
         <Image className='w-8' src='github.svg' alt="" width={30} height={30} />
-        <p className='m-3'>
+        <p className='m-3 font-hand text-3xl'>
           Connect Me</p>
 
       </div>
