0f6b64c89ca1030befac62c10e9b229a0dcbeaf8
Rhishav <rhishh.me@gmail.com>
2024-08-14 16:31:19 +0530

    added-auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google"
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "999594898208-1164o35c41kemv9lplduv584ps94lrmu.apps.googleusercontent.com",
-      clientSecret: "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"
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
 
   ],
