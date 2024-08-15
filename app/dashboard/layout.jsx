420fe81c3161c7c7a893da36717ef0e72116ab4d
Rhishav <rhishh.me@gmail.com>
2024-08-15 17:07:12 +0530

    update

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -8,7 +8,7 @@ import { Addtask } from "../Utils/Add";
 import { Connection } from "../Utils/checkuser";
 
 
-export default function DasboardPage({children}) {
+export default function DasboardPage({ children }) {
   const { data: session } = useSession();
 
   Connection(session?.user?.name);
@@ -16,28 +16,27 @@ export default function DasboardPage({children}) {
     let p = await Data(session?.user?.name);
     Addtask("test", session?.user?.name);
   };
-  
+
 
 
   return (
-    <div className="flex justify-center">
-      <Nav>
-        <div>Welcome User</div>
+    <body className="flex justify-between bg-black">
+      <nav>
+        <div>Welcome {session?.user?.name}</div>
         <div>Your Categories</div>
         <div>1</div>
         <div>2</div>
         <div>3</div>
         <div>4</div>
-      </Nav>
-      <Main>
+      </nav>
+      <main>
         {children}
-      </Main>
-      <Section>
+      </main>
+      <section>
         <div>AI Chat</div>
         <div>Other Tools</div>
-      </Section>
-
-      <button onClick={() => signOut({ callbackUrl: "/" })}>Sign Out</button>
-    </div>
+        <button onClick={() => signOut({ callbackUrl: "/" })}>Sign Out</button>
+      </section>
+    </body>
   )
 }
