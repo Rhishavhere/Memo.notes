2060df09de6316361c0bc4f8576a926f5388ad5e
Rhishav <rhishh.me@gmail.com>
2024-08-16 20:53:28 +0530

    panel updates

---
+++ components/Panel.jsx
@@ -0,0 +1,11 @@
+export default function Panel(){
+  return(
+    <nav className="bg-white">
+      <section className="flex flex-col">
+        <p>Good Morning</p>
+        <button className="bg-purple-600 rounded-md ">Create Note</button>
+        <button>Add To-Do</button>
+      </section>
+    </nav>
+  )
+}
