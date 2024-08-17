531fdf5ea19a3ed22bcb9b57bf2765bd2835fb94
Rhishav <rhishh.me@gmail.com>
2024-08-17 17:01:11 +0530

    working on panel

--- components/Panel.jsx
+++ components/Panel.jsx
@@ -1,10 +1,29 @@
+
+import { useSession } from "next-auth/react"
+
 export default function Panel(){
+
+  const {data:session}=useSession();
+
   return(
-    <nav className="bg-white">
-      <section className="flex flex-col">
-        <p>Good Morning</p>
-        <button className="bg-purple-600 rounded-md ">Create Note</button>
-        <button>Add To-Do</button>
+    <nav className="bg-white/30 h-[100vh] overflow-hidden">
+      <section className="flex flex-col border-b-2 border-black/50 mb-4 p-5">
+        <p className="text-xl">
+          Rhishav{session?.user?.name}</p>
+        <p className="text-sm text-slate-600">
+          rhishh.me@gmail.com</p>
+      </section>
+      <section className="flex flex-col p-4 ">
+        <button className="bg-purple-600 rounded-lg mb-2 text-slate-200 p-2 pl-10 pr-10 ">
+         + New Project</button>
+        <button className="bg-orange-300 rounded-lg mb-6">
+          Add To-Do</button>
+          <nav className="flex flex-col items-start gap-2 ">
+            <button>search</button>
+            <button>Home</button>
+            <button>Notes</button>
+            <button>Tasks</button>
+          </nav>
       </section>
     </nav>
   )
